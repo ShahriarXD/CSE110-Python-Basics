0696aa34526c48062126839f159516dcda1e0c18
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-22 15:25:40 +0600

    Create 1st code.py

---
+++ Function/1st code.py
@@ -0,0 +1,19 @@
+def a(a,b):
+    sum = a+b
+    print (sum)
+
+a(5,6) 
+a(5,116) 
+
+
+
+
+
+def b(*trmairechudi):
+    sum = 0
+    for x in trmairechudi:
+        sum = sum +x
+    print(sum)
+
+b(5,6)    
+b(5,6,11,55)
