e598c8fc76734e32fa0c5b1df9a97992ee17487d
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-23 18:33:51 +0600

    Update List Comprehensions.py

--- Function/List Comprehensions.py
+++ Function/List Comprehensions.py
@@ -1,5 +1,5 @@
 #Comprehension Fuction
-# [expression for item in list]
+#[expression for item in list]
 
 
 SEX = [ 55 ,66 ,88 , 5555]
@@ -10,3 +10,20 @@ print(RESULT)
 SEX1 = [ 55 ,65 ,85 , 5555]
 RESULT1 = [x+5 for x in SEX1]
 print(RESULT1)
+
+SEX1 = [ 55 ,65 ,85 , 5555]
+RESULT1 = [x for x in SEX1 if x>60]
+print(RESULT1)
+
+SEX1 = [ 55 ,65 ,85 , 5555]
+RESULT1 = [x+5 for x in SEX1 if x%2 == 0]
+print(RESULT1)
+
+
+
+
+# =============================OUTPUT=================
+# [55, 66, 88, 5555]
+# [60, 70, 90, 5560]
+# [65, 85, 5555]    
+# []
