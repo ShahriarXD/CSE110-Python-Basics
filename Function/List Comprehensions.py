570780f36cd528bbe4c7d3be825ac81f702e70ed
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-23 18:29:54 +0600

    Create List Comprehensions.py

---
+++ Function/List Comprehensions.py
@@ -0,0 +1,12 @@
+#Comprehension Fuction
+# [expression for item in list]
+
+
+SEX = [ 55 ,66 ,88 , 5555]
+RESULT = [x for x in SEX]
+print(RESULT)
+
+
+SEX1 = [ 55 ,65 ,85 , 5555]
+RESULT1 = [x+5 for x in SEX1]
+print(RESULT1)
