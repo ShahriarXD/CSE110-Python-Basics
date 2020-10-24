ebf46d5d733354618d8857e60d5c004c19e620ee
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-24 20:43:49 +0600

    Create TASK-06.py

---
+++ Assignment/Week-03/TASK-06.py
@@ -0,0 +1,11 @@
+#Write a Python code to displays all the odd numbers between 10 and 50.
+#Output: 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41 43 45 47 49
+
+
+#This code is licensed to Brac University
+#Email: km.shahriar.hossain@g.bracu.ac.bd
+#Author: KM SHAHRIAR HOSSAIN
+#Student Id :20201090
+
+for x in range(11, 50 , 2):
+    print(x , end=" ")
