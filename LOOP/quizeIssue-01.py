e954cc77871a7bd4d2945ee4594468068cc162fc
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-24 16:26:13 +0600

    Create quizeIssue-01.py

---
+++ LOOP/quizeIssue-01.py
@@ -0,0 +1,7 @@
+a = ["ab" , "bc" , "cd" , "de"]
+for x in a:
+    print(x)
+
+a = ["ab" , "bc" , "cd" , "de"]
+for a[2] in a:   #Here the assign value  a[2] will replace the key to previous one
+    print(a)    
