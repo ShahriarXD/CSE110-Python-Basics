b4dac25cde0b0d22f4544eaacc76ed14ee4ecd99
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-24 20:44:56 +0600

    Create TASK-07.py

---
+++ Assignment/Week-03/TASK-07.py
@@ -0,0 +1,48 @@
+# Write a Python code that will calculate the **value of y if the expression** of y is as follows (n is the input):  
+
+# y = 1^2 - 2^2 +3^2 -4^2 +5^2 .........+ n^2
+
+
+# ==========================================================
+
+# **Example01:** 
+
+# **Input:**\
+# 10
+
+# **Output:**\
+# -55
+
+# ==========================================================
+
+# **Example02:** 
+
+# **Input:**\
+# 20
+
+# **Output:**\
+# -210
+
+
+
+#This code is licensed to Brac University
+#Email: km.shahriar.hossain@g.bracu.ac.bd
+#Author: KM SHAHRIAR HOSSAIN
+#Student Id :20201090
+
+n = int(input("Enter a Number: \n"))
+result = 0
+for i in range(1, n + 1) :
+	if (i % 2 == 0): 
+			result = result - pow(i, 2) 
+
+
+result1 = 0          
+for i in range(1, n + 1) :
+	if i % 2 != 0: 
+	    result1 = result1 + pow(i, 2) 
+
+
+final = result + result1
+
+print(final)
