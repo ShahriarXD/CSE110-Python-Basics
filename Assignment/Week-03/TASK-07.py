9fdb44ef41b9b7c5b0e4589de214998287b90eb8
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-26 18:21:48 +0600

    Update TASK-07.py

--- Assignment/Week-03/TASK-07.py
+++ Assignment/Week-03/TASK-07.py
@@ -30,19 +30,39 @@
 #Author: KM SHAHRIAR HOSSAIN
 #Student Id :20201090
 
-n = int(input("Enter a Number: \n"))
-result = 0
-for i in range(1, n + 1) :
-	if (i % 2 == 0): 
-			result = result - pow(i, 2) 
+n = int(input())
+sum = 0
+for x in range (n+1):
+    if x % 2 == 0:
+        sum = sum - x*x
+
+sum1 = 0
+for x in range (n+1):
+    if x % 2 != 0:
+        sum1 = sum1 + x*x
 
 
-result1 = 0          
-for i in range(1, n + 1) :
-	if i % 2 != 0: 
-	    result1 = result1 + pow(i, 2) 
+final_answer = sum + sum1
+print (final_answer)
 
 
-final = result + result1
 
-print(final)
+
+
+# n = int(input("Enter a Number: \n"))
+# result = 0
+# for i in range(1, n + 1) :
+# 	if (i % 2 == 0): 
+# 			result = result - pow(i, 2) 
+
+
+# result1 = 0          
+# for i in range(1, n + 1) :
+# 	if i % 2 != 0: 
+# 	    result1 = result1 + pow(i, 2) 
+
+
+# final = result + result1
+
+# print(final)
+
