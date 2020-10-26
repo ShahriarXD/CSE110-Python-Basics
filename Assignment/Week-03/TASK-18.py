88213531e433ff89619876fd9c8c11e9af45b03b
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-27 04:07:50 +0600

    Create TASK-18.py

---
+++ Assignment/Week-03/TASK-18.py
@@ -0,0 +1,30 @@
+# Write a python program that prints a square of size N using + where N will be given as input.
+
+# =======================================
+
+# Sample Input 5
+
+# Sample Output
+# +++++
+# +++++
+# +++++
+# +++++
+# +++++
+
+# =======================================
+
+# Sample Input 3
+
+# Sample Output
+# +++
+# +++
+# +++
+
+
+#This code is licensed to Brac University
+#Email: km.shahriar.hossain@g.bracu.ac.bd
+#Author: KM SHAHRIAR HOSSAIN
+#Student Id :20201090
+number = int(input())
+for x in range(1 , number+1):
+    print("+" * number)
