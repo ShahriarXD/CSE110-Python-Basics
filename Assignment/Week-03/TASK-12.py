8afc383c8d813d7cf14ab292587dccc36ff21ef6
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-26 19:37:38 +0600

    Create TASK-12.py

---
+++ Assignment/Week-03/TASK-12.py
@@ -0,0 +1,16 @@
+#This code is licensed to Brac University
+#Email: km.shahriar.hossain@g.bracu.ac.bd
+#Author: KM SHAHRIAR HOSSAIN
+#Student Id :20201090
+# n = input()
+# a = len(n)
+# for x in range(0 , a-1):
+#     print(n[x] , end=", ")   
+# print (n [a-1])
+
+n = input()
+a = len(n)
+for x in range(0 , a-1):
+    print(n[x], end=", ")
+else:
+    print(n[x+1]) 
