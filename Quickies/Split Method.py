6170c7319ac40b274bc99da6552f20007b51cafc
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-27 21:30:41 +0600

    Create Split Method.py

---
+++ Quickies/Split Method.py
@@ -0,0 +1,20 @@
+s = (input("Input a binary number: "))
+print(s.split())
+
+s = SEXY
+print(s.split())
+['SEXY']
+
+s = (input("Input a binary number: "))
+print(s.split( ))
+['Tr', 'maire', 'chudi']
+
+
+s = "dasdsa,sdadasdasd,asdasdasd"
+print(s.split(","))
+['dasdsa', 'sdadasdasd', 'asdasdasd']
+
+
+s = "dasdsa#sdadasdasd#asdasdasd"
+print(s.split("#"))
+['dasdsa', 'sdadasdasd', 'asdasdasd']
