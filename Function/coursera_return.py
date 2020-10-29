21e74af28ff6f40ffd8ff7db96780dd4d3c12428
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-29 14:01:58 +0600

    Update coursera_return.py

--- Function/coursera_return.py
+++ Function/coursera_return.py
@@ -15,3 +15,26 @@ print(greeting(),"Princilla")
 #Hello Nafisa   
 #Hello Yanna    
 #Hello Princilla
+
+
+
+def greeting(lang):
+    if lang == "en":
+        return "Hello"
+    elif lang == "fr":
+        return "Holla"
+    elif lang == "jap":
+        return "Watashi"        
+    else:
+        print("Holla Welcome to the club. You are the 13th member of our club.")            
+
+print(greeting("fr"), "NAFISA")
+print(greeting("en"), "Yanna")
+print(greeting("jap"), "Mahir")
+
+
+
+#Output
+#Holla NAFISA
+#Hello Yanna    
+#Watashi Mahir  
