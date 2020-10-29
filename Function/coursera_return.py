c64af0dc3b3128d6b07de8f0cd53c67f5d0cc016
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-29 13:57:13 +0600

    Create coursera_return.py

---
+++ Function/coursera_return.py
@@ -0,0 +1,17 @@
+def greeting():
+    return "Hello"      
+
+print(greeting(),"Schacin")
+print(greeting(),"Sohan")
+print(greeting(),"Samia")
+print(greeting(),"Nafisa")
+print(greeting(),"Yanna")
+print(greeting(),"Princilla")
+
+#Output
+#Hello Schacin
+#Hello Sohan    
+#Hello Samia    
+#Hello Nafisa   
+#Hello Yanna    
+#Hello Princilla
