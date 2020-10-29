f4bdda871e3fb7a9de4fe98688cd999552d98206
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-29 13:38:45 +0600

    Create coursera1.py

---
+++ Function/coursera1.py
@@ -0,0 +1,19 @@
+## Example 1:
+
+a = 66
+def rafin_vai():
+    print("SiR You are the Best.")
+    print("East or West Sir You da best.")
+
+print("Hola!")    
+x = a + 44
+print(x)
+rafin_vai()
+
+
+#Output of the code 
+
+#Hola!
+#110
+#SiR You are the Best.        
+#East or West Sir You da best.
