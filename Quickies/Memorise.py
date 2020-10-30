2caeef352b9206c78cbddd315c6d719086da8d59
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-30 21:55:57 +0600

    Update and rename memorise.py to Memorise.py

---
+++ Quickies/Memorise.py
@@ -0,0 +1,18 @@
+#note here we need to use i we cant use any other.
+
+
+a = 11 , 55 , 55
+print("So the date is: %i / %i / %i"%a)
+
+
+
+n =  5
+n1 = int( "%i%i" % (n,n) )
+print(n1)
+
+#output
+#So the date is: 11 / 55 / 55
+#55
+
+
+
--- Quickies/memorise.py
+++
@@ -1,8 +0,0 @@
-a = 11 , 55 , 55
-print("So the date is: %i / %i / %i"%a)
-
-
-#output
-#So the date is: 11 / 55 / 55
-
-#note here we need to use i we cant use any other.
