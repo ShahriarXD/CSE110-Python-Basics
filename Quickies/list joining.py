3f65372c9775154cb7181e90ec7e6cb6cf3396f8
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-30 16:18:27 +0600

    Create list joining.py

---
+++ Quickies/list joining.py
@@ -0,0 +1,15 @@
+a = ["10" ,"10" ]
+c = "".join(a)
+print(c)
+
+
+#here we cannot take into int so we need to convert 
+#and also we named x into int this is because we cant name int(x) because we cannot assign to call function
+
+
+kisu = [1 , 2 , 33 , 55 ,66 ,550 ,55, 20]
+kisu.reverse()
+a = [str(int) for int in kisu]
+final = "".join(a)
+print(final)
+
