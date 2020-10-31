050d47b7da6fefc0f370b05e176ce0e7fda5b442
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-31 18:50:36 +0600

    Create replace_trick.py

---
+++ Notes/replace_trick.py
@@ -0,0 +1,10 @@
+a = "HeLLO WORLD"
+print(a.replace("L","M",2))
+a = "HeLLO WORLD"
+print(a.replace("L","M",3))
+a = "HeLLO WORLLLLLD"
+print(a.replace("L","M",5))  #here 5 means the code will cahnge 5 times
+
+#HeMMO WORLD
+#HeMMO WORMD    
+#HeMMO WORMMMLLD
