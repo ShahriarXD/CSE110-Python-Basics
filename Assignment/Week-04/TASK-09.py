fa4a2893d3bb57d65303815d6d62dea55660ecd1
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-31 21:08:03 +0600

    Update TASK-09.py

--- Assignment/Week-04/TASK-09.py
+++ Assignment/Week-04/TASK-09.py
@@ -1,7 +1,18 @@
-a = "12345"
-c = a[2::-1]
+# a = "12345"
+# c = a[2::-1]
+# list = []
+# for x in range(len(a)+1):
+#     list.append(a[3:x])
+# a = list[-1]
+# print(f"{c}{a}")
+
+
+
+a = input("")
+b = int(input(""))
+c = a[b::-1]
 list = []
 for x in range(len(a)+1):
-    list.append(a[3:x])
+    list.append(a[b+1:x])
 a = list[-1]
 print(f"{c}{a}")
