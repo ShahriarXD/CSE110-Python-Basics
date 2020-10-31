91249e20180f429fc74f2898567158f6a46897e0
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-10-31 10:48:25 +0600

    Update w3 T5.py

--- Later_Solve/w3 T5.py
+++ Later_Solve/w3 T5.py
@@ -1,3 +1,16 @@
+#PROBLEM 5  (10 points possible)
+#Suppose you are given two strings (they may be empty), s1 and s2. You
+#would like to "lace" these strings together, by successively alternating
+#elements of each string (starting with the first character of s1). If
+#one string is longer than the other, then the remaining elements of the
+#longer string should simply be added at the end of the new string.
+
+#For example, if we lace 'abcd' and 'efghi',
+#we would get the new string: 'aebfcgdhi'.
+
+#Write an iterative procedure, called laceStrings(s1, s2) that does this.
+
+#Note: You will only get ten checks. Use these judiciously.
 
 def laceStrings(s1, s2):
     """
@@ -27,3 +40,4 @@ def laceStrings(s1, s2):
 #Test cases:
 print laceStrings('abcd', 'efgh'), ' Should be aebfcgdh.'
 print laceStrings('abcd', 'efghi'), ' Should be aebfcgdhi.'
+print laceStrings('abcdef', 'ghi'), " Should be agbhcidef."
