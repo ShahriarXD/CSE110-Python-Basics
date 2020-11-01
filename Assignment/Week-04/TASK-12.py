5814bcb980dc0c945f671f55f4153f4e592c505b
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-02 05:31:57 +0600

    Create TASK-12.py

---
+++ Assignment/Week-04/TASK-12.py
@@ -0,0 +1,17 @@
+#Using the same Input given
+a = input("")
+b = a.split(", ")
+for x in b:
+    b.reverse()
+for y in b:
+    print(y)
+
+
+#Using loop and taking input
+# list = []
+# for x in range(1, 5+1):
+#     a = int(input(""))
+#     list.append(a)
+# list.reverse()
+# for x in list:
+#     print(x)   
