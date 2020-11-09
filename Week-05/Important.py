79da49599020bae84181ecd7782311bc9e3664fe
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-09 06:03:42 +0600

    Create Important.py

---
+++ Week-05/Important.py
@@ -0,0 +1,11 @@
+exam_marks = {'Cierra Vega': 175, 'Alden Cantrell': 200, 'Kierra Gentry': 165, 'Pierre Cox': 190}
+for x, y in exam_marks.items():    #
+    print(x , y)
+    
+    
+#===============
+
+Cierra Vega 175   
+Alden Cantrell 200
+Kierra Gentry 165 
+Pierre Cox 190 
