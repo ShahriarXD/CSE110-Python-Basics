30e3b9b34c3707cf27b1c125e51a002783c3862d
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-13 00:32:16 +0600

    Update changing the value of Tuple.py

--- Week-05/changing the value of Tuple.py
+++ Week-05/changing the value of Tuple.py
@@ -6,5 +6,26 @@ t = ('a', 'b', 'c', 'd', 'e')
 t = ('A',) + t[1:]
 print(t)
 
+
+
+
+t = ('a', 'b', 'c', 'd', 'e')
+new_tuple_after_changing = ("BOSS",) + t[0:]
+print(new_tuple_after_changing)
+
+t = ('a', 'b', 'c', 'd', 'e')
+new_tuple_after_changing = ("BOSS",) + t[1:]
+print(new_tuple_after_changing)
+
+
+t = ('a', 'b', 'c', 'd', 'e')
+new_tuple_after_changing = ("BOSS",) + t[4:]
+print(new_tuple_after_changing)
+
 #========Output==========
 ('A', 'b', 'c', 'd', 'e')
+
+
+('BOSS', 'a', 'b', 'c', 'd', 'e')
+('BOSS', 'b', 'c', 'd', 'e')
+('BOSS', 'e')
