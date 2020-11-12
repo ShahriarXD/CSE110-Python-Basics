171b036d3d1d21032599452860c38128c43b295b
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-13 00:40:10 +0600

    Update Reverse Method.py

--- Quickies/Reverse Method.py
+++ Quickies/Reverse Method.py
@@ -8,3 +8,17 @@ print(n)
 
 b = "ALIF"[::-1]
 print (b)
+
+
+
+
+
+txt = 'but soft what light in yonder window breaks'
+words = txt.split()
+t = list()
+for x in words:
+    t.append((len(x), x))
+t.sort(reverse=True)
+print(t)
+#=============================
+[(6, 'yonder'), (6, 'window'), (6, 'breaks'), (5, 'light'), (4, 'what'), (4, 'soft'), (3, 'but'), (2, 'in')]
