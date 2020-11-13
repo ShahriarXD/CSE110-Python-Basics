696802e2b1f77956b7d810889c1c06901e819c7c
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-13 07:19:52 +0600

    Create dict Intro.py

---
+++ Week-05/dict Intro.py
@@ -0,0 +1,16 @@
+a = dict()
+print(a)
+a['one'] = 'uno'
+print(a)
+eng2sp = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+print(eng2sp)
+print(eng2sp["two"])
+print(len(eng2sp))
+print(len(eng2sp["three"]))
+
+# {}
+# {'one': 'uno'}
+# {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+# dos
+# 3
+# 4
