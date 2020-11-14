837b10212bf17de8404b1cc96c32f8a67a56c714
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-14 18:00:58 +0600

    Update Have A Look1.py

--- Assignment/Week-05/Have A Look1.py
+++ Assignment/Week-05/Have A Look1.py
@@ -26,4 +26,4 @@ print(c)
 a = {'Harry':15, 'Draco':8, 'Nevil':19}
 b = {'Ginie':18, 'Luna': 14}
 print(a | b)
-{'Harry': 15, 'Draco': 8, 'Nevil': 19, 'Ginie': 18, 'Luna': 14}
+#{'Harry': 15, 'Draco': 8, 'Nevil': 19, 'Ginie': 18, 'Luna': 14}
