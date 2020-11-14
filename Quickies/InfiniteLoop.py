0c7e56c71621841e608792dc5f100bc21edde405
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-14 23:39:55 +0600

    Create InfiniteLoop.py

---
+++ Quickies/InfiniteLoop.py
@@ -0,0 +1,15 @@
+count = 1
+list = []
+while count == 1:
+    a = input("")
+    if a == "STOP":
+        break
+    list.append(a)
+print(list)
+
+
+
+#=========================================
+count = 1
+while count == 1:
+    print("I LOVE YOU")
