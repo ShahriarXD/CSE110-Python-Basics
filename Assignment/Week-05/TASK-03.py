720c70d1f2db829b9d003421e345ce5b8c9b0142
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-15 01:10:04 +0600

    Create TASK-03.py

---
+++ Assignment/Week-05/TASK-03.py
@@ -0,0 +1,10 @@
+book_info = (
+("Best Mystery & Thriller","The Silent Patient",68,821),
+("Best Horror","The Institute",75,717),
+("Best History & Biography","The five",31,783 ),
+("Best Fiction","The Testaments",98,291)
+)
+b = len(book_info)
+print(f"Size of the tuple is: {b}")
+for x in book_info:
+    print(x)
