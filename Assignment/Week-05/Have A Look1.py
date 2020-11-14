bb9f1d3c86cdbc12c540c4907a2850767201bb41
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-14 17:59:34 +0600

    Update Have A Look1.py

--- Assignment/Week-05/Have A Look1.py
+++ Assignment/Week-05/Have A Look1.py
@@ -1,14 +1,23 @@
 a = {'Harry':15, 'Draco':8, 'Nevil':19}
 b = {'Ginie':18, 'Luna': 14}
 print(a,b)
-
 #{'Harry': 15, 'Draco': 8, 'Nevil': 19} {'Ginie': 18, 'Luna': 14}
+
+
 
 a = {'Harry':15, 'Draco':8, 'Nevil':19}
 b = {'Ginie':18, 'Luna': 14}
 c = a.update(b)
 print(a)
 print(c)
-
 #{'Harry': 15, 'Draco': 8, 'Nevil': 19, 'Ginie': 18, 'Luna': 14}
 #None
+
+
+
+
+a = {'Harry':15, 'Draco':8, 'Nevil':19}
+b = {'Ginie':18, 'Luna': 14}
+c = a.copy()
+print(c)
+#{'Harry': 15, 'Draco': 8, 'Nevil': 19}
