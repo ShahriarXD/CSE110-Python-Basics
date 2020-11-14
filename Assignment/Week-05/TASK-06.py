610cfbfdf5d393f2baf2a3abee9dae989a00b8ea
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-14 17:49:24 +0600

    Create TASK-06.py

---
+++ Assignment/Week-05/TASK-06.py
@@ -0,0 +1,19 @@
+aa = ('a','b','c','d','e','f','g','h')
+bb = reversed(aa)
+print(tuple(bb))
+
+#aa = ('a','b','c','d','e','f','g','h')
+#bb = aa[::-1]
+#print(bb)
+
+
+
+
+# Number_of_Item_you_want = int(input())
+# list = []
+# for x in range(Number_of_Item_you_want):
+#     a = input("")
+#     list.append(a)
+# b = tuple(list)
+# bb = reversed(b)
+# print(tuple(bb))
