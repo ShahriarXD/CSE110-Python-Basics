6ad05ecc5588d3b8047925875d37d0ae593cd258
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-14 18:05:25 +0600

    Update Have A Look1.py

--- Assignment/Week-05/Have A Look1.py
+++ Assignment/Week-05/Have A Look1.py
@@ -27,3 +27,13 @@ a = {'Harry':15, 'Draco':8, 'Nevil':19}
 b = {'Ginie':18, 'Luna': 14}
 print(a | b)
 #{'Harry': 15, 'Draco': 8, 'Nevil': 19, 'Ginie': 18, 'Luna': 14}
+
+
+
+def add(dict1, dict2):
+    result = {**dict1, **dict2}
+    return result
+dict1 = {'Harry':15, 'Draco':8, 'Nevil':19}
+dict2 = {'Ginie':18, 'Luna': 14}
+print(add(dict1,dict2))
+#{'Harry': 15, 'Draco': 8, 'Nevil': 19, 'Ginie': 18, 'Luna': 14}
