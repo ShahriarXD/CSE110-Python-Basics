5afb9dc98dbf19b8bc1a9778844f346772d6c802
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-14 17:44:40 +0600

    Create TASK-04.py

---
+++ Assignment/Week-05/TASK-04.py
@@ -0,0 +1,34 @@
+book_info = (
+("Best Mystery & Thriller","The Silent Patient",68821),
+("Best Horror","The Institute",75717),
+("Best History & Biography","The five",31783 ),
+("Best Fiction","The Testaments",98291)
+)
+(s1,s2,s3,s4)= book_info
+print(s1)
+(Genre,Name,Votes) = s1
+print(f"""{Name} won the '{Genre}' category with {Votes} votes""")
+(Genre,Name,Votes) = s2
+print(f"""{Name} won the '{Genre}' category with {Votes} votes""")
+(Genre,Name,Votes) = s3
+print(f"""{Name} won the '{Genre}' category with {Votes} votes""")
+(Genre,Name,Votes) = s4
+print(f"""{Name} won the '{Genre}' category with {Votes} votes""")
+
+
+
+# book_info = (
+# ("Best Mystery & Thriller","The Silent Patient",68821),
+# ("Best Horror","The Institute",75717),
+# ("Best History & Biography","The five",31783 ),
+# ("Best Fiction","The Testaments",98291)
+# )
+# a,b,c,d = book_info
+# x,y,z=a
+# print(f"""{y} won the '{x}' category with {z} votes""")
+# e,f,g=b
+# print(f"""{f} won the '{e}' category with {g} votes""")
+# h,i,j=c
+# print(f"""{i} won the '{h}' category with {j} votes""")
+# k,l,m=d
+# print(f"""{l} won the '{k}' category with {m} votes""")
