c10d74d18fb1f860bfcbe2eba1ff311da55d8765
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-15 09:53:20 +0600

    Create max from iterable.py

---
+++ Tips & Tricks/min/max from iterable.py
@@ -0,0 +1,10 @@
+#Getting min/max from iterable (with/without specific function).
+
+a = [1, 2, -3]
+b = (1, 2, -3)
+print(min(a))
+print(max(a))
+print(min(b))
+print(max(b))
+print(sum(a))
+print(sum(b))
