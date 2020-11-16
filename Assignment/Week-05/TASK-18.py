1579a5fc0305ef689f53449b81a90e90097beaf6
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-16 06:02:51 +0600

    Update TASK-18.py

--- Assignment/Week-05/TASK-18.py
+++ Assignment/Week-05/TASK-18.py
@@ -1,2 +1,11 @@
 my_dictionary = {'c1': 'Red', 'c2': 'Green', 'c3': None, 'd4': 'Blue'}
-print({k: v for k, v in my_dictionary.items() if v is not None})
+dict = {}
+for x,y in my_dictionary.items():
+    if y is not None:
+        dict[x] = y
+print(dict)
+
+
+# Easy in one line using list comprehension Method
+#my_dictionary = {'c1': 'Red', 'c2': 'Green', 'c3': None, 'd4': 'Blue'}
+#print({k:v for k, v in my_dictionary.items() if v is not None})
