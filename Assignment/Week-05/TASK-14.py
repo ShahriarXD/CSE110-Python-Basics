2338314ac4764d8ed8bf07629aa1d87fe30ea808
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-18 09:48:11 +0600

    Update TASK-14.py

--- Assignment/Week-05/TASK-14.py
+++ Assignment/Week-05/TASK-14.py
@@ -1,3 +1,23 @@
 dict =  {'A': [1,2,3], 'b': ['1', '2'], "c": [4, 5, 6, 7]}
 ctr = sum(map(len, dict.values()))
 print(ctr)
+
+
+
+dict = {'A': [1,2,3], 'b': ['1', '2'], "c": [4, 5, 6, 7]}
+count = 0
+for x , y in dict.items():
+    for z,a in dict.items():
+        count += 1
+print(count)        
+
+
+
+
+dict = {'A': [1,2,3], 'b': ['1', '2'], "c": [4, 5, 6, 7]}
+count = 0
+for x in dict:
+    ff = dict[x]
+    for y in ff:
+        count += 1
+print(count)
