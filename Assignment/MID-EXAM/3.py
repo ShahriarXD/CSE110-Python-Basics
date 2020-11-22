346a85e4b76d136257ce0841436fb6a62e51214d
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-22 10:08:23 +0600

    Create 3.py

---
+++ Assignment/MID-EXAM/3.py
@@ -0,0 +1,15 @@
+import ast
+a = ast.literal_eval(input())
+bb = a.copy()
+b = int(input())
+c = len(a)
+d = c/b
+if b > d:
+    print("Step size is not suitable")
+else:
+    a[0] = a[-1]
+    a[2] = a[-3]
+    xx = a.copy()
+    xx[-1] = bb[0]
+    xx[-3] = bb[2]
+print(xx)
