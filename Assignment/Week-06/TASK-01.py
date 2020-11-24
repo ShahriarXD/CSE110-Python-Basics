7f7cc67bb6fa8404af4f9021624e24a61212b8a9
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-11-25 05:42:47 +0600

    Create TASK-01.py

---
+++ Assignment/Week-06/TASK-01.py
@@ -0,0 +1,15 @@
+def even_checker(value):
+    if value %2 == 0:
+        print("Even!!")
+    else:
+        print("Odd!!")
+even_checker(7)
+
+
+
+# def even_checker(value):
+#     if value %2 == 0:
+#         return "Even!!"
+#     else:
+#         return "Odd!!"
+# print(even_checker(7))
