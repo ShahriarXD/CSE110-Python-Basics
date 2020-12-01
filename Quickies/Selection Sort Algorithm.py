a20cc70b5fb2d47305c9a2dd251236f7d716937b
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-12-01 17:28:53 +0600

    Update Selection Sort Algorithm.py

--- Quickies/Selection Sort Algorithm.py
+++ Quickies/Selection Sort Algorithm.py
@@ -8,3 +8,30 @@ for x in range(len(a)):
     if minvalue != x:
         a[minvalue] , a[x] = a[x] , a[minvalue]
     print(a)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+def sorted(a):
+    for x in range(len(a)):
+        minvalue = x
+        for y in range(x, len(a)):
+            if a[y] > a[minvalue]:
+                minvalue = y
+        if minvalue != x:
+            a[minvalue] , a[x] = a[x] , a[minvalue]
+            print(a)
+    return a
+a = [5,3,8,6,7,2]
+print(sorted(a)) 
