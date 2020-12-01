716ab0559aae1383f245526013137d66b8bbdf5e
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-12-01 17:06:50 +0600

    Create Dual Loop.py

---
+++ Quickies/Dual Loop.py
@@ -0,0 +1,55 @@
+a = [5,3,8,6,7,2]
+
+for x in range(len(a)):
+    minvalue = x
+    for y in range(x+1, len(a)):
+        print(y)
+
+
+1
+2
+3
+4
+5
+2
+3
+4
+5
+3
+4
+5
+4
+5
+5
+
+
+a = [5,3,8,6,7,2]
+
+for x in range(len(a)):
+    minvalue = x
+    for y in range(x, len(a)):
+        print(y)
+
+
+
+0
+1
+2
+3
+4
+5
+1
+2
+3
+4
+5
+2
+3
+4
+5
+3
+4
+5
+4
+5
+5
