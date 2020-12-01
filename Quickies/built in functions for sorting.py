de547a7de0d9f95056ba64820a74c30a18427695
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-12-01 07:37:09 +0600

    Update built in functions for sorting.py

--- Quickies/built in functions for sorting.py
+++ Quickies/built in functions for sorting.py
@@ -13,3 +13,26 @@ print(c)
 [-2, 312, 131, 1, 15, -99, 1, 11, 5, 15, 12, 1]
 [-99, -2, 1, 1, 1, 5, 11, 12, 15, 15, 131, 312]
 [312, 131, 15, 15, 12, 11, 5, 1, 1, 1, -2, -99]
+
+
+
+
+
+
+
+
+
+
+
+inventory = {
+    "Chips": 10,
+    "Mr Twist": 55,
+    "Alloz" : 66,
+    "Layz" : 55
+}
+final = sorted(inventory)
+print(final)
+dict = {}
+for x in final:
+    dict[x] = inventory[x]
+print(dict)
