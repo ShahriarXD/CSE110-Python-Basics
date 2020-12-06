1a8eada3eb36756c42fa89e0fffac91ac042b728
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-12-06 13:52:48 +0600

    Create 1.py

---
+++ Week 08/1.py
@@ -0,0 +1,22 @@
+a = 6
+b = 2
+try:
+	print(a)
+	x = a/b
+	print(x)
+except Exception:
+	print("not possible Sir")
+a = 6
+b = 0
+try:
+	x = a/b
+	print(x)
+except Exception:
+	print("not possible Sir")	
+  
+  
+  
+  
+6
+3.0
+not possible Sir
