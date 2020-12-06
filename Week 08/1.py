af3b7a43ee7a880423e661a2de026ed54ab066cc
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2020-12-06 17:00:43 +0600

    Update 1.py

--- Week 08/1.py
+++ Week 08/1.py
@@ -28,3 +28,48 @@ finally:
 Done
 not possible Sir
 Done
+
+
+
+
+
+
+
+a = 6
+b = 0
+try:
+	print(a)
+	x = a/b
+	print(x)
+except Exception:
+	print("not possible Sir")
+else:
+    print("No exception Occured")
+finally:
+    print("Done")
+
+a = 6
+b = 2
+try:
+	print(a)
+	x = a/b
+	print(x)
+except Exception:
+	print("not possible Sir")
+else:
+    print("No exception Occured")
+finally:
+    print("Done")
+
+
+
+
+
+
+6
+not possible Sir    
+Done
+6
+3.0
+No exception Occured
+Done
